2fc4c2210a5475a8116e8d83b18000d06c138111
chechetov <dr.dan79@gmail.com>
2023-07-26 20:58:23 +0300

    redo organize method to store all matches in hash

--- leaguetable.rb
+++ leaguetable.rb
@@ -12,45 +12,21 @@ class LeagueTable
 		@match_list
 	end
 
-	def organize
-		matches_array = @match_list.matches.map { |string| string.split("-") }
-
-		p matches_array
-
-		matches_results = {}
-
-		matches_array.each do |match_result|
-				p match_result
-
-				# Regular expression to match team score
-				regex = /\d+/
-				# Extracting team names and scores using regular expressions
-				team_name_1 = match_result[0].gsub(regex, "").strip
-				team_score_1 = match_result[0].scan(regex).first.to_i
-
-				team_name_2 = match_result[1].gsub(regex, "").strip
-				team_score_2 = match_result[1].scan(regex).first.to_i
-
-				matches_results[team_name_1] = team_score_1
-				matches_results[team_name_2] = team_score_2
-		end
-
-		p matches_results
-
-	end
-
 	def get_goals_for
-
 	end
 
 end
 
+
+
+
+
 lt = LeagueTable.new
 lt.matches.push("Man Utd 3 - 0 Liverpool")
-#lt.matches.push("Liverpool 1 - 1 Man Utd")
+lt.matches.push("Liverpool 1 - 1 Man Utd")
 
-puts lt.matches.inspect
-lt.organize
+#puts lt.matches.inspect
+#lt.organize
 
 
 
--- matchlist.rb
+++ matchlist.rb
@@ -1,14 +1,45 @@
-# ChatGPT thanks for this custom library
+# Some parts are made with usage of ChatGPT
+# But many are not.
 
 class MatchList
+
   attr_reader :matches
 
   def initialize
     @matches = []
+    @all_matches_hash = {}
+    @match_number = 0
   end
 
   def push(match_result)
     @matches.push(match_result)
+    @match_number += 1
+    organize(match_result)
+  end
+
+  def organize(match_result)
+  	match_result_arr = match_result.split("-")
+  	match_result_hash = {}
+
+  	match_result_arr.each do |team_result|
+  		# Regular expression to match team score
+  		regex = /\d+/
+  		# Extracting team names and scores using regular expressions
+  		team_name_1 = team_result.gsub(regex, "").strip
+  		team_score_1 = team_result.scan(regex).first.to_i
+
+  		team_name_2 = team_result.gsub(regex, "").strip
+  		team_score_2 = team_result.scan(regex).first.to_i
+
+  		match_result_hash[team_name_1] = team_score_1
+  		match_result_hash[team_name_2] = team_score_2
+  	end
+    @all_matches_hash["match#{@match_number}"] = match_result_hash
+    p @all_matches_hash
+  end
+
+  def get_last
+  	@matches[-1]
   end
 
 end
