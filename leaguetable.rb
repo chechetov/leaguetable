ae6d6a1f00229fa2471c7d7adbe2b0fe16cd34e0
chechetov <dr.dan79@gmail.com>
2023-07-27 19:19:02 +0300

    fix goals and points table

--- leaguetable.rb
+++ leaguetable.rb
@@ -10,131 +10,19 @@ class LeagueTable
 		@match_list
 	end
 
-	def get_results_table
-		@match_list.all_matches_hash
+	def get_goals_table
+		@match_list.all_matches_goals_table
 	end
 
-	# Returns the no. of points a team has, 0 by default
-	def get_points(team_name)
-		points = 0
-		# Goes through matches results and 
-		# If a team is present then adds to score
-
-		# Since Ruby 1.9.1 Hash preserves the insertion order
-		# https://stackoverflow.com/questions/12155383/how-to-get-hash-values-by-position-in-ruby
-		get_results_table.each do |match|
-			if match[1].has_key?(team_name)
-				points = points + match[1][team_name]
-			end
-		end
-		points
-	end
-
-	# Returns the no. of goals a team has scored, 0 by default
-	# As far as I Understand, it is the same as get_goals_for.
-	def get_goals_for(team_name)
-		get_points(team_name)
-	end
-
-	# Returns the no. of goals a team has conceeded (had scored against them), 0 by default
-	def get_goals_against(team_name)
-		points = 0
-		get_results_table.each do |match|
-			if match[1].has_key?(team_name)
-				# Get values for all keys except team_name
-				points_conceeded = match[1].reject { |key, _| key == team_name }.values
-				# Here should be only one number but just in case I sum array instead
-				points = points + points_conceeded.sum
-			end
-		end
-		points
-	end
-
-	# Return the no. of goals a team has scored minus the no. of goals a team has conceeded, 0 by default
-	def get_goal_difference(team_name)
-		get_goals_for(team_name) - get_goals_against(team_name)
-	end
-
-	def get_wins(team_name)
-		wins = 0
-		get_results_table.each do |match|
-			if match[1].has_key?(team_name)
-				# Win counted if team has 3 points
-				if match[1][team_name] == 3
-					wins = wins + 1
-				end
-			end
-		end
-		wins
-	end
-
-	def get_draws(team_name)
-		draws = 0
-		get_results_table.each do |match|
-			if match[1].has_key?(team_name)
-				# Draw counted if team has 1 point
-				if match[1][team_name] == 1
-					draws = draws + 1
-				end
-			end
-		end
-		draws
-	end
-
-	def get_losses(team_name)
-		losses = 0
-		get_results_table.each do |match|
-			if match[1].has_key?(team_name)
-				# Loss is counted if team has 0 points
-				if match[1][team_name] == 0
-					losses = losses +1
-				end
-			end
-		end
-		losses
+	def get_points_table
+		@match_list.all_matches_points_table
 	end
 
 end
 
+
 lt = LeagueTable.new
+lt.matches.push("Man Utd 3 - 2 Liverpool")
+puts "Goals_table: #{lt.get_goals_table}" 
+puts "Points_table: #{lt.get_points_table}"
 
-lt.matches.push("Man Utd 3 - 0 Liverpool")
-puts lt.get_losses("Liverpool") # 1
-#puts lt.get_goal_difference("Liverpool")
-#puts lt.get_goals_against("Liverpool")
-
-lt.matches.push("Liverpool 0 - 3 Man Utd")
-puts lt.get_losses("Liverpool") # 2
-#puts lt.get_goal_difference("Liverpool")
-#puts lt.get_goals_against("Liverpool")
-
-lt.matches.push("Crawley 0  - 1 Colchester")
-puts lt.get_losses("Colchester") # 0
-
-
-lt.matches.push("Crawley 1  - 1 Colchester")
-puts lt.get_losses("Colchester") # 0
-
-
-
-
-#lt.get_results_table
-#p lt.get_points("Man Utd")
-#p lt.get_points("Liverpool")
-
-#puts lt.matches.inspect
-#lt.organize
-
-
-
-# puts lt.get_goals_for("Man Utd") #  => 3
-# puts lt.get_points("Man Utd")  # => 3
-# puts lt.get_points("Liverpool") # => 0
-# puts lt.get_goal_difference("Liverpool") # => -3
-
-# lt.matches.push("Liverpool 1 - 1 Man Utd")
-# puts lt.get_goals_for("Man Utd") # => 4
-# puts lt.get_points("Man Utd") # => 4
-# puts lt.get_points("Liverpool") # => 1
-# puts lt.get_goals_against("Man Utd") # => 1
-# puts lt.get_points("Tottenham") # => 0
--- matchlist.rb
+++ matchlist.rb
@@ -1,14 +1,16 @@
-# Some parts are made with usage of ChatGPT
-# But many are not.
+# Some (tiny) parts are made with usage of ChatGPT
+# But many are not
 
 class MatchList
 
   attr_reader :matches
-  attr_reader :all_matches_hash
+  attr_reader :all_matches_goals_table
+  attr_reader :all_matches_points_table
 
   def initialize
     @matches = []
-    @all_matches_hash = {}
+    @all_matches_goals_table = {}
+    @all_matches_points_table = {}
     @match_number = 0
   end
 
@@ -16,28 +18,69 @@ class MatchList
   def push(match_result)
     @matches.push(match_result)
     @match_number += 1
-    organize(match_result)
+    parsed_match = parse_match_result(match_result)
+    add_to_goals_table(parsed_match)
+    add_to_points_table(parsed_match)
   end
 
-  def organize(match_result)
-  	match_result_arr = match_result.split("-")
-  	match_result_hash = {}
+  def parse_match_result(match_result)
 
-  	match_result_arr.each do |team_result|
-  		# Regular expression to match team score
-  		regex = /\d+/
-  		# Extracting team names and scores using regular expressions
-  		team_name_1 = team_result.gsub(regex, "").strip
-  		team_score_1 = team_result.scan(regex).first.to_i
+    match_result_arr = match_result.split("-")
+    match_result_hash = {}
 
-  		team_name_2 = team_result.gsub(regex, "").strip
-  		team_score_2 = team_result.scan(regex).first.to_i
+    match_result_arr.each do |team_result|
+      # Regular expression to match team score
+      regex = /\d+/
+      # Extracting team names and scores using regular expressions
+      team_name_1 = team_result.gsub(regex, "").strip
+      team_score_1 = team_result.scan(regex).first.to_i
 
-  		match_result_hash[team_name_1] = team_score_1
-  		match_result_hash[team_name_2] = team_score_2
-  	end
-    @all_matches_hash["match#{@match_number}"] = match_result_hash
+      team_name_2 = team_result.gsub(regex, "").strip
+      team_score_2 = team_result.scan(regex).first.to_i
+
+      match_result_hash[team_name_1] = team_score_1
+      match_result_hash[team_name_2] = team_score_2
+    end
+    match_result_hash
   end
 
+  def add_to_goals_table(match_result)
+    # Adds parsed match result into a Goals table
+  	# {'matchN' => {'team1' => 'goals1', 'team2'=>'goals2'}}
+    @all_matches_goals_table["match#{@match_number}"] = match_result
+  end
 
+  def add_to_points_table(match_result)
+    # Calculates points for given match result and adds into Points table
+    # # {'matchN' => {'team1' => 'points1', 'team2'=>'points2'}}
+
+    points_hash = {}
+
+    # Corresponds to order of insertion
+    # ["Team A", "Team B"]
+    teams = match_result.keys
+    # ["Goals A", "Goals B"]
+    goals = match_result.values
+
+    # Goals A > Goals B => Team A has 3 points, Team B has 0 points
+    if goals[0] > goals[1]
+      points_hash[teams[0]] = 3
+      points_hash[teams[1]] = 0
+    end
+
+    # Goals B > Goals A => Team B has 3 points, Team A has 0 points
+    if goals[0] < goals[1]
+      points_hash[teams[1]] = 3
+      points_hash[teams[0]] = 0
+    end
+
+    # Goals A == Goals B => 1 point for each team
+    if goals[0] == goals[1]
+      points_hash[teams[0]] = 1
+      points_hash[teams[1]] = 1
+    end
+
+    @all_matches_points_table["match#{@match_number}"] = points_hash
+
+  end
 end
