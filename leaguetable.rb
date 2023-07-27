4bc5f2fb085a6f007f919e427583ab8ef75fd88b
chechetov <dr.dan79@gmail.com>
2023-07-27 19:42:47 +0300

    get_goal_differrence

--- leaguetable.rb
+++ leaguetable.rb
@@ -40,6 +40,23 @@ class LeagueTable
 		goals
 	end
 
+	def get_goals_against(team_name)
+		goals = 0
+		get_goals_table.each do |match|
+			if match[1].has_key?(team_name)
+			# Get values for all keys except team_name
+			goals_conceeded = match[1].reject { |key, _| key == team_name }.values
+			# Here should be only one number but just in case I sum array into integer
+			goals = goals + goals_conceeded.sum
+		end
+	end
+	goals
+	end
+
+	def get_goal_difference(team_name)
+		get_goals_for(team_name) - get_goals_against(team_name)
+	end
+
 end
 
 lt = LeagueTable.new
@@ -48,10 +65,37 @@ lt.matches.push("Man Utd 3 - 2 Liverpool")
 puts "Goals_table: #{lt.get_goals_table}" 
 puts "Points_table: #{lt.get_points_table}"
 
-puts "Get points for Liverpool: #{lt.get_points("Liverpool")}" # 0
-puts "Get points for Manchester: #{lt.get_points("Man Utd")}" # 3
+puts "Points for Liverpool: #{lt.get_points("Liverpool")}" # 0
+puts "Points for Manchester: #{lt.get_points("Man Utd")}" # 3
 
-puts "Get goals for Liverpool: #{lt.get_goals_for("Liverpool")}" # 2
-puts "Get goals for Manchester: #{lt.get_goals_for("Man Utd")}" # 3
+puts "Goals for Liverpool: #{lt.get_goals_for("Liverpool")}" # 2
+puts "Goals for Manchester: #{lt.get_goals_for("Man Utd")}" # 3
+
+puts "Goals against Liverpool: #{lt.get_goals_against("Liverpool")}" # 3
+puts "Goals against Manchester: #{lt.get_goals_against("Man Utd")}" # 2
+
+puts "Goals difference Liverpool: #{lt.get_goal_difference("Liverpool")}" # -1
+puts "Goals difference Manchester: #{lt.get_goal_difference("Manchester")}" # 1
+
+
+lt.matches.push("Man Utd 1 - 2 Liverpool")
+
+puts "Goals_table: #{lt.get_goals_table}" 
+puts "Points_table: #{lt.get_points_table}"
+
+puts "Points for Liverpool: #{lt.get_points("Liverpool")}" # 3
+puts "Points for Manchester: #{lt.get_points("Man Utd")}" # 3
+
+puts "Goals for Liverpool: #{lt.get_goals_for("Liverpool")}" # 4
+puts "Goals for Manchester: #{lt.get_goals_for("Man Utd")}" # 4
+
+puts "Goals against Liverpool: #{lt.get_goals_against("Liverpool")}" # 4
+puts "Goals against Manchester: #{lt.get_goals_against("Man Utd")}" # 4
+
+puts "Goals difference Liverpool: #{lt.get_goal_difference("Liverpool")}" # 0
+puts "Goals difference Manchester: #{lt.get_goal_difference("Manchester")}" # 0
+
+
+
 
 
